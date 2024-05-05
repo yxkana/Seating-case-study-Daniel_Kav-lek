38bd6b0386ab050a45a425c7b39ce83289b18af9
Kanna <yxkana52@gmail.com>
2024-05-05 23:33:19 +0200

    Add hook for Event and Seatings api call

---
+++ src/hooks/useGetEvent.ts
@@ -0,0 +1,56 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { api } from "@/lib/constants";
+
+export interface eventData {
+  eventId: string;
+  namePub: string;
+  description: string;
+  currencyIso: string;
+  dateFrom: Date;
+  dateTo: Date;
+  headerImageUrl: string;
+  place: string;
+}
+
+export interface seatsData {
+  ticketTypes: [
+    {
+      id: string;
+      name: string;
+      price: number;
+    }
+  ];
+  seatRows: [
+    {
+      seatRow: number;
+      seats: [
+        {
+          seatId: string;
+          place: number;
+          ticketTypeId: number;
+        }
+      ];
+    }
+  ];
+}
+
+export const useGetEvent = () => {
+  return useQuery({
+    queryKey: ["getEvent"],
+    queryFn: async () => {
+      const data = await axios.get(`${api}/event`).then(async (results) => {
+        const { data } = await axios.get(
+          `${api}/event-tickets?eventId=${results.data.eventId}`
+        );
+
+        return {
+          event: results.data as eventData,
+          seatings: data as seatsData,
+        };
+      });
+
+      return data;
+    },
+  });
+};
---
+++ src/lib/constants.ts
@@ -0,0 +1,2 @@
+export const api =
+  "https://nfctron-frontend-seating-case-study-2024.vercel.app";
